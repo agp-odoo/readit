057e7102ab752ba70c30f2ea1f37fa161c55fcd4
Ana-Gabriel <agp@odoo.com>
2020-01-10 10:50:29 -0800

    [ADD] readit: add proper category tag to manifest

--- readit/__manifest__.py
+++ readit/__manifest__.py
@@ -31,7 +31,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'license': 'AGPL-3',
-    'category': 'Readit',
+    'category': 'Discuss',
     'version': '1.0',
 
     # any module necessary for this one to work correctly
